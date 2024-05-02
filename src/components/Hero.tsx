9aa7be48d551390883c961f0fdd8c8b6300f7912
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-02 13:02:36 -0300

    Mudança

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,43 @@
 // Hero.tsx
+'use client'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function Hero() {
-    return (
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  };
 
-    );
+  return (
+    <div className="w-full h-1/2 relative">
+      <Slider {...sliderSettings}>
+        <div>
+          <img src="/image1.jpg" className="bg-white w-full h-full object-cover" alt="Slide 1" />
+        </div>
+        <div>
+          <img src="/image2.jpg" className="bg-white w-full h-full object-cover" alt="Slide 2" />
+        </div>
+        <div>
+          <img src="/image3.jpg" className="bg-white w-full h-full object-cover" alt="Slide 3" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,70 @@
 // Nav.tsx
+'use client'
+import { useState, useEffect } from 'react';
 
 export default function Nav() {
-    return (
+    const [activeSection, setActiveSection] = useState('home');
 
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition < window.innerHeight - 100) {
+            setActiveSection('home');
+        } else if (scrollPosition < 2 * window.innerHeight - 100) {
+            setActiveSection('sobre');
+        } else if (scrollPosition < 3 * window.innerHeight - 100) {
+            setActiveSection('locais');
+        } else {
+            setActiveSection('cambio');
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollToSection = (id: string) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    return (
+        <nav className="bg-transparent p-8 fixed w-full z-20">
+            <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+                <div className="flex items-center ml-0 lg:ml-8"></div>
+                <div className="md:flex text-white space-x-6 lg:space-x-12 mt-4 md:mt-0">
+                    <button
+                        className={`text-base md:text-lg ${activeSection === 'home' ? 'bg-white text-black px-3 rounded-full' : ''}`}
+                        onClick={() => scrollToSection('home')}
+                    >
+                        Home
+                    </button>
+                    <button
+                        className={`text-base md:text-lg ${activeSection === 'sobre' ? 'bg-white text-black px-3 rounded-full' : ''}`}
+                        onClick={() => scrollToSection('sobre')}
+                    >
+                        Sobre
+                    </button>
+                    <button
+                        className={`text-base md:text-lg ${activeSection === 'locais' ? 'bg-white text-black px-3 rounded-full' : ''}`}
+                        onClick={() => scrollToSection('locais')}
+                    >
+                        Locais
+                    </button>
+                    <button
+                        className={`text-base md:text-lg ${activeSection === 'cambio' ? 'bg-white text-black px-3 rounded-full' : ''}`}
+                        onClick={() => scrollToSection('cambio')}
+                    >
+                        Câmbio
+                    </button>
+                </div>
+                <button className="bg-none hidden text-white lg:block rounded-full text-base md:text-5xl font-bold px-4 mt-4 md:mt-0 mr-0 lg:mr-8"></button>
+            </div>
+        </nav>
     );
 }
