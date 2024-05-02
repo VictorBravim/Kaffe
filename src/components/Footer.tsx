ec2f2f0e0c4ccdc95f68b3043797bc414e5c8aaf
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-02 20:39:42 -0300

    Mudança

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,7 +41,7 @@ export default function Footer() {
                 <div className="flex items-center mb-2 lg:mb-0">
                     <Image src={logo} alt="Texto" width={150} height={32} />
                 </div>
-                <div className="md:flex space-x-8 lg:space-x-12 mt-4 md:mt-0">
+                <div className="md:flex space-x-2 lg:space-x-12 mt-4 md:mt-0">
                     <button
                         className={`text-base md:text-lg ${activeSection === 'home' ? 'bg-transparent text-white py-2 border-b-2 border-white' : ''}`}
                         onClick={() => scrollToSection('home')}>
